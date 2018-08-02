9dc2891e89e5ac01433839f48432230678fbeaeb
JulianPoh <julianpoh01@gmail.com>
2018-08-02 22:48:37 +0800

    initial commit - hot mess

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
 class ListItem extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: '',
-      items: []
-    };
-  }
-
-  onChange = (event) => {
-    this.setState({ term: event.target.value });
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <form className="App" onSubmit={this.onSubmit}>
-          <input value={this.state.term} onChange={this.onChange} />
-          <button>Submit</button>
-        </form>
-        <List items={this.state.items} />
-      </div>
-    );
-  }
-}
-
-const List = props => (
-  <ul>
-    {
-      props.items.map((item, index) => <li key={index}>{item}</li>)
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        term: '',
+        items: []
+      };
     }
-  </ul>
-);
+  
+    onChange = (event) => {
+      this.setState({ term: event.target.value });
+    }
+  
+    onSubmit = (event) => {
+      event.preventDefault();
+      this.setState({
+        term: '',
+        items: [...this.state.items, this.state.term]
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <form className="App" onSubmit={this.onSubmit}>
+            <input value={this.state.term} onChange={this.onChange} />
+            <button>Submit</button>
+          </form>
+          <List items={this.state.items} />
+        </div>
+      );
+    }
+  }
 
 ReactDOM.render(
     <List/>,
